12208b86fa9c7b0a7ed51052e78dc4be18ff97b9
Ryan <ryan.lavigne@mymail.champlain.edu>
2023-02-09 15:05:07 -0500

    move to desktop

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -54,15 +54,21 @@ long duration;
 // }
 
 void setup() {
-  pinMode(TRIGGER_PIN, OUTPUT); // Sets the trigPin as an Output
-  pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an Input
-  pinMode(BUZZER_PIN, OUTPUT); // Sets the buzzerPin as an Output
-  pinMode(GREEN_LED, OUTPUT); // Sets the greenLedPin as an Output
-  pinMode(RED_LED, OUTPUT); // Sets the redLedPin as an Output
-  Serial.begin(9600); // Starts the serial communication
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
+void setlights(int redlight, int greenlight) {
+  digitalWrite(RED_LED, redlight);
+  digitalWrite(GREEN_LED, greenlight);
+}
+
+
+int readdata() {
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
@@ -72,17 +78,101 @@ void loop() {
   duration = pulseIn(ECHO_PIN, HIGH);
   int distance = duration * 0.034 / 2;
 
+  return distance;
+}
+
+// int findaverage(int distance) {
+//   int average = 0;
+//   int sum = 0;
+//   int readings[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
+//   return average;
+// }
+
+// int findaverage(int distance) {
+//   //find the avergae of the last 10 readings
+//   // and if one is more than 150% different from the average ignore it
+//   // and find the average of the remaining 9
+
+//   int average = 0;
+//   int sum = 0;
+//   int readings[10];
+//   for (int i = 0; i < 10; i++) {
+//     readings[i] = 0;
+//   }
+//   int current = 0;
+//   int count = 0;
+
+//   while (count < 10) {
+//     if (distance < MAX_DISTANCE) {
+//       readings[current] = distance;
+//       current = (current + 1) % 150;
+//       count++;
+//     }
+//   }
+
+//   for (int i = 0; i < 10; i++) {
+//     sum += readings[i];
+//   }
+//   average = sum / 10;
+
+//   //check if any of the readings are more than 10% different from average
+//   for (int i = 0; i < 10; i++) {
+//     if (readings[i] > (average * 1.5) || readings[i] < (average * 0)) {
+//       readings[i] = 0;
+//     }
+//   }
+
+//   //find the average of the remaining readings
+//   sum = 0;
+//   count = 0;
+//   for (int i = 0; i < 10; i++) {
+//     if (readings[i] != 0) {
+//       sum += readings[i];
+//       count++;
+//     }
+//   }
+//   average = sum / count;
+
+//   Serial.print("Average: ");
+//   Serial.println(average);
+
+//   return average;
+// }
+
+void loop() {
+  // digitalWrite(TRIGGER_PIN, LOW);
+  // delayMicroseconds(2);
+  // digitalWrite(TRIGGER_PIN, HIGH);
+  // delayMicroseconds(10);
+  // digitalWrite(TRIGGER_PIN, LOW);
+
+  // duration = pulseIn(ECHO_PIN, HIGH);
+  // int distance = duration * 0.034 / 2;
+
+  // readdata();
+
+  // delay(50);
+
+  // findaverage();
+
+  long distance = readdata();
+
+  // findaverage(distance);
+
   if(distance < 15 || distance > 150) {
     tone(BUZZER_PIN, 440);
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, LOW);
-  } else if(distance > MAX_DISTANCE){
+    setlights(HIGH, LOW);
+  } else if(distance < MAX_DISTANCE){
     noTone(BUZZER_PIN);
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
+    setlights(LOW, HIGH);
+  } else if (distance > MAX_DISTANCE) {
+    noTone(BUZZER_PIN);
+    setlights(LOW, HIGH);
   }
-
   Serial.print("Distance: ");
   Serial.println(distance);
-  
+
+  // delay(50);
 }
