c32f0658fa9185e0e11d3c93da3080ee7a6a9269
Ryan LaVigne <61425102+RyanLavigne421@users.noreply.github.com>
2023-02-09 22:14:35 -0500

    Finished

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,59 +1,35 @@
-#include <Arduino.h>
-#include <NewPing.h>
+#include <LiquidCrystal.h>
 
+//LEDs
 #define GREEN_LED 2
 #define RED_LED 3
+
+//LCD
+#define BACKLIGHT 13
+#define CONTRAST 12
+
+//Buzzer
 #define BUZZER_PIN  8
+
+//UltraSonic Sensor
 #define TRIGGER_PIN  9
 #define ECHO_PIN     10
-#define MAX_DISTANCE 400 // Maximum distance we want to measure (in centimeters).
 
-long duration;
-// int distanceCm, distanceInch;
+long duration; // Duration used to calculate distance
+int distanceCm; // Distance measured
+int distanceIn; // Distance measured
 
-// NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-
-// void setup() {
-//   pinMode(BUZZER_PIN, OUTPUT);
-//   pinMode(GREEN_LED, OUTPUT);
-//   pinMode(RED_LED, OUTPUT);
-//   Serial.begin(9600);
-// }
-
-// void loop() {
-//   // delay(50);
-//   // digitalWrite(TRIGGER_PIN, LOW);
-//   // delayMicroseconds(2);
-//   // digitalWrite(TRIGGER_PIN, HIGH);
-//   // delayMicroseconds(10);
-//   // digitalWrite(TRIGGER_PIN, LOW);
-//   // duration = pulseIn(ECHO_PIN, HIGH);
-//   // distanceCm = duration * 0.034 / 2;
-//   // distanceInch = duration * 0.0133 / 2;
-//   // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-
-//   distanceCm = sonar.ping_cm(); // Send ping, get distance in cm and print result (0 = outside set distance range)
-//   distanceInch = sonar.ping_in(); // Send ping, get distance in cm and print result (0 = outside set distance range)
-
-//   Serial.print("Distance: ");
-//   Serial.print(distanceCm);
-//   Serial.println("Cm");
-
-//   if(distanceCm < 15 && distanceCm > 300) {
-//     tone(BUZZER_PIN, 440);
-//     digitalWrite(RED_LED, HIGH);
-//     digitalWrite(GREEN_LED, LOW);
-//   } 
-//     noTone(BUZZER_PIN);
-//     digitalWrite(RED_LED, LOW);
-//     digitalWrite(GREEN_LED, HIGH);
-  
-//   // digitalWrite(RED_LED, LOW);
-//   // digitalWrite(GREEN_LED, HIGH);
-//   // noTone(BUZZER_PIN);
-// }
+LiquidCrystal lcd(13, 12, 4, 5, 6, 7); // LCD Shield
 
 void setup() {
+  lcd.begin(16, 2); // Initialize the LCD
+  lcd.clear(); // Clear the LCD
+
+  pinMode(BACKLIGHT, OUTPUT);
+  analogWrite(BACKLIGHT, 210);
+  pinMode(CONTRAST, OUTPUT);
+  analogWrite(CONTRAST, 40);
+
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
@@ -62,13 +38,15 @@ void setup() {
   Serial.begin(9600);
 }
 
-void setlights(int redlight, int greenlight) {
-  digitalWrite(RED_LED, redlight);
-  digitalWrite(GREEN_LED, greenlight);
+void setlights(int redLightState, int greenLightState) {
+  //Sets the Red and Green lights to the correct state
+  digitalWrite(RED_LED, redLightState);
+  digitalWrite(GREEN_LED, greenLightState);
 }
 
+void loop() {
+  lcd.clear();
 
-int readdata() {
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
@@ -76,103 +54,25 @@ int readdata() {
   digitalWrite(TRIGGER_PIN, LOW);
 
   duration = pulseIn(ECHO_PIN, HIGH);
-  int distance = duration * 0.034 / 2;
+  distanceCm = duration * 0.034 / 2;
+  distanceIn = duration * 0.0133 / 2;
 
-  return distance;
-}
+  // Prints distances on LCD Screen
+  lcd.setCursor(0, 0);
+  lcd.print("Distance: " + String(distanceCm) + "Cm");
+  lcd.setCursor(0, 1);
+  lcd.print("Distance: " + String(distanceIn) + "In");
 
-// int findaverage(int distance) {
-//   int average = 0;
-//   int sum = 0;
-//   int readings[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-
-//   return average;
-// }
-
-// int findaverage(int distance) {
-//   //find the avergae of the last 10 readings
-//   // and if one is more than 150% different from the average ignore it
-//   // and find the average of the remaining 9
-
-//   int average = 0;
-//   int sum = 0;
-//   int readings[10];
-//   for (int i = 0; i < 10; i++) {
-//     readings[i] = 0;
-//   }
-//   int current = 0;
-//   int count = 0;
-
-//   while (count < 10) {
-//     if (distance < MAX_DISTANCE) {
-//       readings[current] = distance;
-//       current = (current + 1) % 150;
-//       count++;
-//     }
-//   }
-
-//   for (int i = 0; i < 10; i++) {
-//     sum += readings[i];
-//   }
-//   average = sum / 10;
-
-//   //check if any of the readings are more than 10% different from average
-//   for (int i = 0; i < 10; i++) {
-//     if (readings[i] > (average * 1.5) || readings[i] < (average * 0)) {
-//       readings[i] = 0;
-//     }
-//   }
-
-//   //find the average of the remaining readings
-//   sum = 0;
-//   count = 0;
-//   for (int i = 0; i < 10; i++) {
-//     if (readings[i] != 0) {
-//       sum += readings[i];
-//       count++;
-//     }
-//   }
-//   average = sum / count;
-
-//   Serial.print("Average: ");
-//   Serial.println(average);
-
-//   return average;
-// }
-
-void loop() {
-  // digitalWrite(TRIGGER_PIN, LOW);
-  // delayMicroseconds(2);
-  // digitalWrite(TRIGGER_PIN, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(TRIGGER_PIN, LOW);
-
-  // duration = pulseIn(ECHO_PIN, HIGH);
-  // int distance = duration * 0.034 / 2;
-
-  // readdata();
-
-  // delay(50);
-
-  // findaverage();
-
-  long distance = readdata();
-
-  // findaverage(distance);
-
-  if(distance < 15 || distance > 150) {
+  if(distanceCm < 15 || distanceCm > 150) { // Too close or Too far
     tone(BUZZER_PIN, 440);
     setlights(HIGH, LOW);
-  } else if(distance < MAX_DISTANCE){
+  } else if(distanceCm < 1000 || distanceCm > 1200){ // UltraSonic Sensor gets junk data sometimes
     noTone(BUZZER_PIN);
     setlights(LOW, HIGH);
-  } else if (distance > MAX_DISTANCE) {
+  } else { // Safe distance
     noTone(BUZZER_PIN);
     setlights(LOW, HIGH);
   }
   Serial.print("Distance: ");
-  Serial.println(distance);
-
-  // delay(50);
+  Serial.println(distanceCm);
 }
